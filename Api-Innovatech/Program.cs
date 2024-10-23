029b51cc6dcd85ddeec02132ba1adbb83e22c04d
jeanci-jb <jeanci.99jb@gmail.com>
2024-10-23 11:09:36 -0500

    Se realizo los cambios para "Proveedor"

--- Api-Innovatech/Program.cs
+++ Api-Innovatech/Program.cs
@@ -3,19 +3,18 @@ using Api_Innovatech.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Confi
-
+// Configuración de la cadena de conexión
 var Configuracion = new Configuracion(builder.Configuration.GetConnectionString("innovatech_500"));
 builder.Services.AddSingleton(Configuracion);
 
+// Registro de servicios
 builder.Services.AddScoped<IProducto, CrudProducto>();
-
+builder.Services.AddScoped<IProveedor, CrudProveedor>(); // Agrega esta línea
 
 var app = builder.Build();
 
@@ -27,9 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- Api-Innovatech/obj/Debug/net8.0/Fruteria-LaFrutera-Api.AssemblyInfo.cs
+++ Api-Innovatech/obj/Debug/net8.0/Fruteria-LaFrutera-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fruteria-LaFrutera-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bf63e9fd2a6b68c5290eed77d14f8c9c28c7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858c6c3b5a0cdf184714fabdc407538b56f7e228")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fruteria-LaFrutera-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fruteria-LaFrutera-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
